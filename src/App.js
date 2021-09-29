24068c3759b99dae3dc2552f344d084898ec2e48
Jack Hubert <jphubert93@gmail.com>
2021-09-29 12:02:27 -0600

    firebase testing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jack Hubert Development
         </a>
       </header>
     </div>
